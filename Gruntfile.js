e7950a8b2186abc4f061e251799db3a96b1ec5e3
Francisco Pinchentti <francisco.pinchentti@nan-labs.com>
2015-03-17 14:49:26 -0300

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,93 @@
+// @see http://gruntjs.com/configuring-tasks
+
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        less: {
+            dev: {
+                files:{'app/dist/styles/styles.css':'app/styles/style.less'}
+            }
+        },
+
+        connect: {
+            dev: {
+                options: {
+                    port: 3000,
+                    base:'./app/',
+                    keepalive:false
+                }
+            }
+        },
+
+        jshint: {
+            dev: [
+                'Gruntfile.js',
+                'app/components/**/*.js',
+                'app/main.js'
+            ]
+        },
+
+        concat: {
+            options: {
+                separator: '\n',
+            },
+            dist: {
+                src: [
+                    'app/components/**/*.js',
+                    'app/main.js',
+                ],
+                dest: 'app/dist/js/built.js'
+            },
+            libs: {
+                src: [
+                    'bower_components/jquery/dist/jquery.js',
+                    'bower_components/bootstrap/dist/js/bootstrap.min.js',
+                    'app/bower_components/angular/angular.js',
+                    'app/bower_components/angular-ui-router/release/angular-ui-router.js'
+                ],
+                dest: 'app/dist/js/libs.js'
+            }
+        },
+
+        watch: {
+            connect: {     // task to watch
+                options: { // Live reload is now specific to this task
+                    livereload: true
+                },
+                files: [   // Files to livereload on
+                    "**/*.js",
+                    "**/*.html",
+                    "!app/dist/js/libs.js",
+                    "!app/dist/js/built.js"
+                ],
+                tasks: ['concat'] // @bug works but executes multiple times on each change
+            },
+            less: { // less files should be compiled after changes
+                files: '**/*.less',
+                tasks: ['less'],
+                options: {
+                    livereload: true,
+                },
+            }
+        }
+
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+
+    grunt.loadNpmTasks('grunt-contrib-connect');
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('dev', ['less:dev', 'concat', 'connect:dev', 'jshint:dev']);
+
+    grunt.registerTask('default', ['dev', 'watch']);
+
+};
